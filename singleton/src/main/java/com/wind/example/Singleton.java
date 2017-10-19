eb6c4f5d6773bdc19bd633e93b16204686cedac9
followwwind <18771933975@163.com>
2017-10-19 12:23:13 +0800

    添加单例模块

---
+++ singleton/src/main/java/com/wind/example/Singleton.java
@@ -0,0 +1,33 @@
+package com.wind.example;
+
+/**
+ * 单例模式 双重校验锁,在JDK1.5之后，双重检查锁定才能够正常达到单例效果
+ * 单例模式一般分为五种 懒汉，恶汉，双重校验锁，枚举和静态内部类
+ *
+ * 如果涉及到反序列化创建对象时使用枚举的方式来实现单例
+ * @author wind
+ */
+public class Singleton {
+
+    private volatile static Singleton singleton;
+    private Singleton (){
+
+    }
+    public static Singleton getSingleton() {
+        if (singleton == null) {
+            synchronized (Singleton.class) {
+                if (singleton == null) {
+                    singleton = new Singleton();
+                }
+            }
+        }
+        return singleton;
+    }
+
+}
+
+
+
+
+
+
---
+++ singleton/src/main/java/com/wind/example/Singleton6.java
@@ -0,0 +1,15 @@
+package com.wind.example;
+
+/**
+ * 枚举
+ * 这种方式是Effective Java作者Josh Bloch 提倡的方式，它不仅能避免多线程同步问题，而且还能防止反序列化重新创建新的对象，
+ * 可谓是很坚强的壁垒啊，不过，个人认为由于1.5中才加入enum特性，
+ * 用这种方式写不免让人感觉生疏，在实际工作中，我也很少看见有人这么写过。
+ * @author wind
+ */
+public enum Singleton6 {
+    /**
+     *
+     */
+    INSTANCE;
+}
